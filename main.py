ff0cc12a61b1455730dc2cbf643f5248cfa15bdc
Pon <is02n15ds@bk.ru>
2025-05-03 19:32:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from settings import TEL_TOKEN
+from app.handlers import router
+from services.random_image import get_random_imgur_image
+
+bot = Bot(token=TEL_TOKEN)
+dp = Dispatcher()
+
+# Функция, которая обновляет список изображений каждую 1 минут.
+async def update_images_periodically():
+    from data.data import current_images  # Импортируем здесь, чтобы избежать циклического импорта
+    while True:
+        try:
+            new_images = []
+            # Получаем 5 новых изображения
+            for _ in range(5):
+                await asyncio.sleep(1)
+                img = await get_random_imgur_image()
+                if img:
+                    new_images.append(img)
+            current_images[:] = new_images  # Изменяем список на месте
+            print(f"Изображения были обновлены!")
+
+        except Exception as e:
+            print(f"Ошибка обновления изображения: {e}")
+
+        await asyncio.sleep(120)
+
+
+async def main():
+    dp.include_router(router=router)
+
+    # Создаем задачу для обновления изображений
+    update_task = asyncio.create_task(update_images_periodically())
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        update_task.cancel()
+        try:
+            await update_task
+        except asyncio.CancelledError:
+            pass
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Выход из программы!')
---
+++ settings.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv("config.env")
+
+TEL_TOKEN = os.getenv("BOT_TOKEN")
+CLIENT_ID_IMGUR = os.getenv("API_TOKEN")
+WHITE_LIST_CHATS = [int(x) for x in os.getenv("WHITE_LIST_CHATS").split(";") if x.strip()]
+WHITE_LIST_USERS = [int(x) for x in os.getenv("WHITE_LIST_USERS").split(";") if x.strip()]
+
+WHITE_LIST_ENABLE=False
+MAX_CHARS=4096
+MAX_REQUESTS=5
+ROLLBACK_TIME=120
