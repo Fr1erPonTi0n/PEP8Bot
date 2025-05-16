1bc86c7a1aa44d16a18df2bd9ee390d41cdbcd3a
Pon <is02n15ds@bk.ru>
2025-05-16 12:20:26 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ load_dotenv("config.env")
 
 TEL_TOKEN = os.getenv("BOT_TOKEN")
 CLIENT_ID_IMGUR = os.getenv("API_TOKEN")
-WHITE_LIST_CHATS = [int(x) for x in os.getenv("WHITE_LIST_CHATS").split(";") if x.strip()]
-WHITE_LIST_USERS = [int(x) for x in os.getenv("WHITE_LIST_USERS").split(";") if x.strip()]
+WHITE_LIST_CHATS = [int(x) for x in os.getenv("WHITE_LIST_CHATS").split(",") if x.strip()]
+WHITE_LIST_USERS = [int(x) for x in os.getenv("WHITE_LIST_USERS").split(",") if x.strip()]
 
 WHITE_LIST_ENABLE=False
 MAX_CHARS=4096
